43dbc2fa96b1cbd2a2ee20db5163f862db4b52b2
ajita singh <ajitavsingh_8@yahoo.com>
2017-12-11 11:47:39 -0500

    test added

---
+++ weatherdaily/tests/test_models.py
@@ -0,0 +1,27 @@
+from django.test import TestCase
+from django.db import IntegrityError
+from weatherdaily.models import Weather
+
+
+class WeatherModelTest(TestCase):
+
+
+    @classmethod
+    def setUpTestData(cls):
+        # set up objects to test data
+        Weather.objects.create(email='test@test.com', location='Anchorage, AK')
+
+    def test_email_label(self):
+        weather = Weather.objects.get(id=1)
+        field_label = weather._meta.get_field('email').verbose_name
+        self.assertEquals(field_label, 'email')
+
+    def test_location_email(self):
+        weather = Weather.objects.get(id=1)
+        field_label = weather._meta.get_field('location').verbose_name
+        self.assertEquals(field_label, 'location')
+
+    def test_location_max_length(self):
+        weather = Weather.objects.get(id=1)
+        max_length = weather._meta.get_field('location').max_length
+        self.assertEquals(max_length, 50)
